0565b58d0fa0663ca8dd4eb6e31c3b9d9a0392e6
Geetika jindal <geetikajindal78@gmail.com>
2020-07-12 16:21:03 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,33 +1,36 @@
-variable subscription_id {
+# variable subscription_id {
     
-}
-variable client_id {
+# }
+# variable client_id {
     
-}
-variable client_secret {
+# }
+# variable client_secret {
 
-}
-variable tenant_id {
+# }
+# variable tenant_id {
     
-}
+# }
 
-
+#location
 variable "location" {
     description = "The location where resources are created"
     default     = "East US"
     
 }
+
+#resource_group_name
 variable "resource_group_name" {
     description = "The name of the resource group in which the resources are created"
     default     = "DeployingWebserverInAzure"
 }
 
+#value
 variable "value" {
     description = "Number of VM instances"
     default     = "2"
 }
 
-
+#tag
 variable "tags" {
     type        = map(string)
     default     = {
@@ -36,6 +39,7 @@ variable "tags" {
 
 }
 
+#prefix
 variable  "prefix"{
     default    = "DeployingWebserverInAzure"
 }
